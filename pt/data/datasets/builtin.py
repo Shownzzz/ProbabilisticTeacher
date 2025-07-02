13dd8bd741558c362e29f5ada0735e6a5499db33
Shownzzz <qq14525457@gmail.com>
2025-07-03 05:35:45 +0800

    修改config命名, 修改builtin

--- pt/data/datasets/builtin.py
+++ pt/data/datasets/builtin.py
@@ -20,7 +20,7 @@ from fvcore.common.timer import Timer
 from fvcore.common.file_io import PathManager
 import io
 import logging
-from detectron2.data.datasets.pascal_voc import register_pascal_voc
+#from detectron2.data.datasets.pascal_voc import register_pascal_voc
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +37,84 @@ _SPLITS_COCO_FORMAT["coco"] = {
     ),
 }
 
+# from detectron2.data.datasets.pascal_voc import register_pascal_voc 抓出來修改 讀取png
+import xml.etree.ElementTree as ET
+from typing import List, Tuple, Union
+import numpy as np
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.structures import BoxMode
+from detectron2.utils.file_io import PathManager
+
+__all__ = ["load_voc_instances", "register_pascal_voc"]
+
+# fmt: off
+CLASS_NAMES = (
+    "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat",
+    "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person",
+    "pottedplant", "sheep", "sofa", "train", "tvmonitor"
+)
+# fmt: on
+
+
+def load_voc_instances(dirname: str, split: str, class_names: Union[List[str], Tuple[str, ...]]):
+    """
+    Load Pascal VOC detection annotations to Detectron2 format.
+
+    Args:
+        dirname: Contain "Annotations", "ImageSets", "JPEGImages"
+        split (str): one of "train", "test", "val", "trainval"
+        class_names: list or tuple of class names
+    """
+    with PathManager.open(os.path.join(dirname, "ImageSets", "Main", split + ".txt")) as f:
+        fileids = np.loadtxt(f, dtype=str)
+
+    # Needs to read many small annotation files. Makes sense at local
+    annotation_dirname = PathManager.get_local_path(os.path.join(dirname, "Annotations/"))
+    dicts = []
+    for fileid in fileids:
+        anno_file = os.path.join(annotation_dirname, fileid + ".xml")
+        jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".jpg")
+        if not os.path.exists(jpeg_file):
+            jpeg_file = os.path.join(dirname, "JPEGImages", fileid + ".png")    #讀不到jpg檔案就讀png檔案
+
+        with PathManager.open(anno_file) as f:
+            tree = ET.parse(f)
+
+        r = {
+            "file_name": jpeg_file,
+            "image_id": fileid,
+            "height": int(tree.findall("./size/height")[0].text),
+            "width": int(tree.findall("./size/width")[0].text),
+        }
+        instances = []
+
+        for obj in tree.findall("object"):
+            cls = obj.find("name").text
+            # We include "difficult" samples in training.
+            # Based on limited experiments, they don't hurt accuracy.
+            # difficult = int(obj.find("difficult").text)
+            # if difficult == 1:
+            # continue
+            bbox = obj.find("bndbox")
+            bbox = [float(bbox.find(x).text) for x in ["xmin", "ymin", "xmax", "ymax"]]
+            # Original annotations are integers in the range [1, W or H]
+            # Assuming they mean 1-based pixel indices (inclusive),
+            # a box with annotation (xmin=1, xmax=W) covers the whole image.
+            # In coordinate space this is represented by (xmin=0, xmax=W)
+            bbox[0] -= 1.0
+            bbox[1] -= 1.0
+            instances.append(
+                {"category_id": class_names.index(cls), "bbox": bbox, "bbox_mode": BoxMode.XYXY_ABS}
+            )
+        r["annotations"] = instances
+        dicts.append(r)
+    return dicts
+
+def register_pascal_voc(name, dirname, split, year, class_names=CLASS_NAMES):
+    DatasetCatalog.register(name, lambda: load_voc_instances(dirname, split, class_names))
+    MetadataCatalog.get(name).set(
+        thing_classes=list(class_names), dirname=dirname, year=year, split=split
+    )
 
 def register_coco_unlabel(root):
     for _, splits_per_dataset in _SPLITS_COCO_FORMAT.items():
@@ -119,11 +197,11 @@ def load_coco_unlabel_json(
 # ==== Predefined splits for PASCAL VOC ===========
 def register_all_pascal_voc(root):
     SPLITS = [
-        ("VOC2007_citytrain", 'data/VOC2007_citytrain', "train", 8),
+        ("VOC2007_citytrain", 'data/VOC2007_citytrain', "trainval", 8),
         ("VOC2007_foggytrain", 'data/VOC2007_foggytrain', "train", 8),
         ("VOC2007_foggytrain_plus", 'data/VOC2007_foggytrain', "train_plus", 8), # 加上長尾類別truck, train, motorcycle
         ("VOC2007_foggytrain_all", 'data/VOC2007_foggytrain', "train_all", 8), # 加上所有類別
-        ("VOC2007_foggyval", 'data/VOC2007_foggyval', "val", 8),
+        ("VOC2007_foggyval", 'data/VOC2007_foggyval', "test", 8),
         ("VOC2007_citytrain1", 'data/VOC2007_citytrain1', "train", 1),
         ("VOC2007_cityval1", 'data/VOC2007_cityval1', "val", 1),
         ("VOC2007_bddtrain", 'data/VOC2007_bddtrain', "train", 8),
